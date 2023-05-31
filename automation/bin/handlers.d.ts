831675f16e3177ff7f8c26aa09b11db5f49fc4ec
phillip edwards <phillipedwards02@gmail.com>
2023-05-30 19:38:10 -0700

    initial commit of automation api and pulumi program

--- automation/bin/handlers.d.ts
+++
@@ -1,6 +0,0 @@
-import * as express from "express";
-export declare const createHandler: express.RequestHandler;
-export declare const updateHandler: express.RequestHandler;
-export declare const listHandler: express.RequestHandler;
-export declare const getHandler: express.RequestHandler;
-export declare const deleteHandler: express.RequestHandler;
--- automation/bin/storage/websites/gcpWebsite.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=gcpWebsite.js.map
